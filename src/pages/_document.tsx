f61e32d07fddd21bc702d9889d85025e96ce17a0
Bruno Ferreira <fs.brunoferreira@gmail.com>
2022-05-18 13:53:06 -0300

    implement navbar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;700;800&display=swap" rel="stylesheet" />
+          <link rel="stylesheet" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
